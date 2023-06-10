95ad6bc7c2433b2affafe286b0d326432b5b99bf
Romulus09 <italotenorio01@gmail.com>
2023-06-10 14:30:36 -0300

    Adds Tela Inicial

---
+++ Math Tetris/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    
+	public void play_game(){
+	
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+
+        }
+
+    public void Quit_Game()
+    {
+        Debug.Log("QUIT !!!");
+        Application.Quit();
+    }
+}
