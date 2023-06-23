5a66302a1d7180b76abb86d47f8ccc2c4cc14c6a
Romulus09 <italotenorio01@gmail.com>
2023-06-23 12:41:46 -0300

    corrige implementação do tratamento de peças bloqueadas

--- Math Tetris/Assets/Scripts/Smalltetrisblock.cs
+++ Math Tetris/Assets/Scripts/Smalltetrisblock.cs
@@ -12,24 +12,20 @@ public class Smalltetrisblock : MonoBehaviour
     private float falltimer;
     int res;
     Boolean res_already_added = true;
-    Boolean is_Blocked = false;
-    Expression_Control expression_Control;
 
 
-    private void Awake()
-    {
-        /*  expression_Control = GameObject.Find("PeçaP").GetComponent<Expression_Control>();  */
-        
-    }
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
 
-       /* res = UnityEngine.Random.Range(0, 9);
-        expression_Control._Results.Add(res);// adiciona resultado da peça na lista
-        text.text = res.ToString();  */
+
+        res = UnityEngine.Random.Range(0, 9);
+        Expression_Control._instance._Results.Add(res);// adiciona resultado da peça na lista
+        text.text = res.ToString();
     }
 
     // Update is called once per frame
@@ -46,29 +42,27 @@ public class Smalltetrisblock : MonoBehaviour
         if (down_hit)
         {
             hasFallen = true;
-            
+
         }
 
 
-       /* if (hasFallen)
+        if (hasFallen)
         {
             if (up_hit)// verifica se há uma outra peça pequena em cima dessa
             {
-                is_Blocked = true; // no futuro, fazer com que is_Blocked = false quando a peça de cima é destruída
+                if (res_already_added) // verifica se há uma outra peça pequena em cima dessa e se o resultado está na lista
+                {
+                    Expression_Control._instance._Results.Remove(res);
+                    res_already_added = false;
+                }
             }
-            if (is_Blocked && res_already_added) // verifica se há uma outra peça pequena em cima dessa e se o resultado está na lista
+            else if (!res_already_added)
             {
-                expression_Control._Results.Remove(res);
-                res_already_added = false;
-
-            }
-            if (!is_Blocked && !res_already_added)
-            {
-                expression_Control._Results.Add(res);
+                Expression_Control._instance._Results.Add(res);
                 res_already_added = true;
-            } 
-        } */
+            }
+        }
     }
 
- 
+
 }
